13379de57577ade6c056e7b1e1688f987b138982
Mayur_Jain <mayur87545@gmail.com>
2020-12-15 23:01:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,22 +25,22 @@ def show_plot(iteration,loss):
     plt.show()
 
 
-# def view_dissimilarity(testDirectory):
-#     folder_dataset_test = dset.ImageFolder(root=testDirectory)
-#     siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset_test,
-#                                             transform=transforms.Compose([transforms.Resize((100,100)),
-#                                                                           transforms.ToTensor()
-#                                                                           ])
-#                                            ,should_invert=False)
+def view_dissimilarity(testDirectory):
+    folder_dataset_test = dset.ImageFolder(root=testDirectory)
+    siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset_test,
+                                            transform=transforms.Compose([transforms.Resize((100,100)),
+                                                                          transforms.ToTensor()
+                                                                          ])
+                                           ,should_invert=False)
 
-#     test_dataloader = DataLoader(siamese_dataset,num_workers=6,batch_size=1,shuffle=True)
-#     dataiter = iter(test_dataloader)
-#     x0,_,_ = next(dataiter)
+    test_dataloader = DataLoader(siamese_dataset,num_workers=6,batch_size=1,shuffle=True)
+    dataiter = iter(test_dataloader)
+    x0,_,_ = next(dataiter)
 
-#     for i in range(4):
-#         _,x1,label2 = next(dataiter)
-#         concatenated = torch.cat((x0,x1),0)
-#         model.eval()
-#         output1,output2 = model(Variable(x0).cuda(),Variable(x1).cuda())
-#         euclidean_distance = F.pairwise_distance(output1, output2)
-#         imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.item()))
+    for i in range(4):
+        _,x1,label2 = next(dataiter)
+        concatenated = torch.cat((x0,x1),0)
+        model.eval()
+        output1,output2 = model(Variable(x0).cuda(),Variable(x1).cuda())
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.item()))
